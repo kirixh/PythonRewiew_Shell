b672c7a13f9531060d8ecb586b150a2a650c46e5
kirixh <piatkovskii.k@phystech.edu>
2021-04-04 23:32:09 +0300

    filled README.md and main.py, removed abstract_commands.py from source dir

---
+++ main.py
@@ -0,0 +1,37 @@
+from Commands_Sources.Ls import Ls
+from Commands_Sources.Pwd import Pwd
+from Commands_Sources.Cd import Cd
+from Commands_Sources.Cp import Cp
+from Commands_Sources.Mv import Mv
+from Commands_Sources.Rm import Rm
+from Commands_Sources.Rmdir import Rmdir
+from Commands_Sources.Mkdir import Mkdir
+print("Hello! This is shell on python. Possible commands:\n"
+      "ls - Output a list of files and directories for the current directory.\n"
+      "pwd - Print the full path for the current directory.\n"
+      "cd <path> - Change current directory by a relative or absolute path.\n"
+      "cp <filename1> <filename2> - Copy filename1 into filename2.\n"
+      "mv <filename1> <filename2> - Move the filename1 to filename2.\n"
+      "rm <filename> - Delete the file.\n"
+      "rmdir <dirname> - Delete the directory if it's empty.\n"
+      "mkdir <dirname> - Make directory if it didn't exist.\n"
+      "exit - Stop the program.\n")
+ls = Ls()
+pwd = Pwd()
+cd = Cd()
+cp = Cp()
+mv = Mv()
+rm = Rm()
+rmdir = Rmdir()
+mkdir = Mkdir()
+commands = {'ls': ls, 'pwd': pwd, 'cd': cd, 'cp': cp,
+            'mv': mv, 'rm': rm, 'rmdir': rmdir, 'mkdir': mkdir}
+while True:
+    cm = input().split()
+    if cm[0] == 'exit':
+        break
+    try:
+        commands[cm[0]].command(*cm[1:])
+    except Exception as exc:
+        print(exc)
+
